8aaa5e633c2932a117cfd145deb3e5eb37a10f09
Guest 01 <guest01@alunos.sc.senac.br>
2022-06-21 20:50:39 -0300

    Modulos typescript

---
+++ typescript/aula2106/calculos.ts
@@ -0,0 +1,12 @@
+/*function somar(n1: number, n2:number): number{
+    return n1 + n2
+}
+
+function subtrair(n1: number, n2:number): number{
+    return n1 - n2
+}*/
+
+import somar from './modulomatematica'
+
+var z = somar(20, 10)
+
