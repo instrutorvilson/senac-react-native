804c07ac33c22abefbf6e880c78bb09847d72d6e
Guest 01 <guest01@alunos.sc.senac.br>
2022-06-22 20:38:17 -0300

    Exercicio criação classe pessoa

---
+++ typescript/aula POO/Pessoa.ts
@@ -0,0 +1,34 @@
+class Pessoa{
+    id: number
+    nome:string
+    fone:string
+    email:string
+    peso:number
+    altura:number
+
+    constructor(id:number, nome:string, 
+                fone:string, email:string, 
+                peso: number, altura:number){
+      this.id = id
+      this.nome = nome
+      this.fone = fone
+      this.email = email 
+      this.altura = altura
+      this.peso = peso 
+    }
+
+    imprimir(): void{
+      console.log(`Id: ${this.id}`)
+      console.log(`Nome: ${this.nome}`)
+      console.log(`Fone: ${this.fone}`)
+      console.log(`Email: ${this.email}`)
+      console.log('')
+    }
+
+    calcularIMC():number{
+       let imc = (this.peso/(this.altura*this.altura)).toFixed(2);
+       return parseFloat(imc)
+    }
+}
+
+export default Pessoa
---
+++ typescript/aula POO/testeClassePessoa.ts
@@ -0,0 +1,25 @@
+import Pessoa from "./Pessoa"
+
+let jose = new Pessoa(1,'José da silva','123','jose@gmail.com',90,1.68);
+/*console.log(jose.email)
+jose.fone = '707070'
+console.log(jose.fone)
+*/
+console.log(jose.calcularIMC())
+jose.imprimir()
+
+
+let maria = new Pessoa(2,'maria','345','maria@gmail.com',75,1.75)
+//console.log(maria.nome)
+console.log(maria.calcularIMC())
+maria.imprimir()
+
+
+//criar um array de pessoas
+/*let pessoas: Pessoa[] = []
+pessoas.push(jose)
+pessoas.push(maria)
+pessoas.push(new Pessoa(3,'Antonia','2345','antonia@gmail.com'))
+
+//console.log(pessoas)
+//pessoas.forEach((p) => p.imprimir());*/
